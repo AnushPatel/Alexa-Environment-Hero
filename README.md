c9e9d28733a0e28901d4032184e0228f8600aa2f
Raiyan Sayeed <yoboi7585@gmail.com>
2018-09-16 21:08:06 -0400

    updated readme, story and changed index interaction speech

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 [![icon-above-font.png](https://s8.postimg.cc/6sh3zf4n9/icon-above-font.png)](https://postimg.cc/image/50o54ila9/)
 
-Stuck in a polluted earth, it is your mission to find a way out. Journey through towns and find clues that take your 
+Stuck in a polluted earth, it is your mission to find a way out. Journey through towns and find clues that teach you about the importance of living an environmentally friendly life. 
 
 Made for the Alexa Tech For Good Competition 2018.
 By Md Raiyan bin Sayeed and Anush Patel!
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -82,7 +82,7 @@ const handlers = {
     if (this.event.session.attributes['room'] === undefined) {
       // you just started so you are in the first room
       this.event.session.attributes['room'] = $twine[0]['$']['pid'];
-      speechOutput = `<audio src='https://s3.amazonaws.com/ask-soundlibrary/nature/amzn_sfx_strong_wind_whistling_01.mp3'/> Welcome to ${story.replace('.html','')},the story of an envrionmentally friendly lifetime! Stories will be told to you and you will have to make the right decisions. Lets start your game. `;
+      speechOutput = `<audio src='https://s3.amazonaws.com/ask-soundlibrary/nature/amzn_sfx_strong_wind_whistling_01.mp3'/> Welcome to ${story.replace('.html','')},the story of an envrionmentally friendly lifetime! Stories will be told to you and you will have to make the right decisions. For the best experience, play with your eyes closed and let your mind do the rest! Without further ado, lets start your game. `;
     }
 
     var room = currentRoom(this.event);
@@ -208,7 +208,7 @@ const handlers = {
     this.emit('CompletelyExit');
   },
   'CompletelyExit': function() {
-    var speechOutput = 'Goodbye.';
+    var speechOutput = 'Thanks for spending your precious time to become a better you. Please consider leaving a review on the app store: it means a lot!';
     if (TableName) {
       speechOutput = `Your progress has been saved. ${speechOutput}`;
     }
